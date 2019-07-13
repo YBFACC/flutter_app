24ec1ec271ddbc1692460f35721c1b619dc89fd0
454447236@qq.com <454447236@qq.com>
2019-07-13 23:31:09 +0800

    活动入口

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/dao/home_dao.dart';
+import 'package:flutter_app/model/common_model.dart';
+import 'package:flutter_app/model/grid_nav_model.dart';
+import 'package:flutter_app/model/home_model.dart';
+import 'package:flutter_app/widget/local_nav.dart';
+import 'package:flutter_app/widget/sub_nav.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
+import 'package:flutter_app/widget/grid_nav.dart';
+
+const APPBAR_SCROLL_OFFSET = 100;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List _imageUrls = [
+    'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
+    'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg',
+    'https://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg'
+  ];
+  double appBarAlpha = 0;
+
+  //String resultString = "";
+  List<CommonModel> localNavList = [];
+  List<CommonModel> subNavList = [];
+  GridNavModel gridNavModel;
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  _onSCroll(offset) {
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+    print(appBarAlpha);
+  }
+
+  loadData() async {
+//    HomeDao.fetch().then((result) {
+//      setState(() {
+//        resultString = json.encode(result);
+//      });
+//    }).catchError((e){
+//      resultString=e.toString();
+//    });
+    try {
+      HomeModel model = await HomeDao.fetch();
+      setState(() {
+        //resultString = json.encode(model.config);
+        localNavList = model.localNavList;
+        gridNavModel = model.gridNav;
+        subNavList = model.subNavList;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfff2f2f2),
+      body: Stack(
+        children: <Widget>[
+          MediaQuery.removePadding(
+            removeTop: true,
+            context: context,
+            child: NotificationListener(
+              onNotification: (scrollNotification) {
+                if (scrollNotification is ScrollUpdateNotification &&
+                    scrollNotification.depth == 0) {
+                  _onSCroll(scrollNotification.metrics.pixels);
+                }
+              },
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 160,
+                    child: Swiper(
+                      itemCount: _imageUrls.length,
+                      autoplay: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(
+                          _imageUrls[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                      pagination: SwiperPagination(),
+                    ),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+                      child: LocalNav(localNavList: localNavList)),
+                  Padding(
+                      padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+                      child: GridNav(gridNavModel: gridNavModel)),
+                  Padding(
+                      padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+                      child: SubNav(subNavList: subNavList)),
+                  Container(
+                    height: 800,
+                    child: ListTile(
+                      title: Text('resultString'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Opacity(
+            opacity: appBarAlpha,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 40),
+                  child: Text('首页'),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
