7e51e83d615b3b2096c08070d5514628fa8fc0a4
454447236@qq.com <454447236@qq.com>
2019-07-18 14:51:08 +0800

    SearchDao

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,6 +7,7 @@ import 'package:flutter_app/model/sales_box_model.dart';
 import 'package:flutter_app/widget/loading_container.dart';
 import 'package:flutter_app/widget/local_nav.dart';
 import 'package:flutter_app/widget/sales_box.dart';
+import 'package:flutter_app/widget/search_bar.dart';
 import 'package:flutter_app/widget/sub_nav.dart';
 import 'package:flutter_app/widget/webview.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
@@ -14,6 +15,7 @@ import 'dart:convert';
 import 'package:flutter_app/widget/grid_nav.dart';
 
 const APPBAR_SCROLL_OFFSET = 100;
+const SEARCH_BSR_DEFAULT_TEXT = '网红打卡地 景点 酒店 美食';
 
 class HomePage extends StatefulWidget {
   @override
@@ -113,7 +115,8 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-  Widget get _listView{
+
+  Widget get _listView {
     return ListView(
       children: <Widget>[
         _banner,
@@ -134,22 +137,45 @@ class _HomePageState extends State<HomePage> {
       ],
     );
   }
-  Widget get _appBar{
-    return Opacity(
-      opacity: appBarAlpha,
-      child: Container(
-        height: 80,
-        decoration: BoxDecoration(color: Colors.white),
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.only(top: 40),
-            child: Text('首页'),
+
+  Widget get _appBar {
+    return Column(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [Color(0x66000000), Colors.transparent],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          )),
+          child: Container(
+            padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+            height: 80.0,
+            decoration: BoxDecoration(
+              color: Color.fromARGB((appBarAlpha * 255).toInt(), 255, 255, 255),
+            ),
+            child: SearchBar(
+              searchBarType: appBarAlpha > 0.2
+                  ? SearchBarType.homeLight
+                  : SearchBarType.home,
+              inputBoxClick: _jumpToSearch,
+              speakClick: _jumpToSpeak,
+              defaultText: SEARCH_BSR_DEFAULT_TEXT,
+              leftButtonClick: () {},
+            ),
           ),
         ),
-      ),
+        Container(
+          height: appBarAlpha>0.2?0.5:0,
+          decoration: BoxDecoration(
+            boxShadow: [BoxShadow(color: Colors.black12,blurRadius: 0.5)]
+          ),
+        )
+      ],
     );
   }
-  Widget get _banner{
+
+  Widget get _banner {
     return Container(
       height: 160,
       child: Swiper(
@@ -180,4 +206,8 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+  _jumpToSearch() {}
+
+  _jumpToSpeak() {}
 }
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_app/dao/search_dao.dart';
+import 'package:flutter_app/model/search_model.dart';
+import 'package:flutter_app/widget/search_bar.dart';
+const URL='https://m.ctrip.com/restapi/h5api/searchapp/search?source=mobileweb&action=autocomplete&contentType=json&keyword=';
 class SearchPage extends StatefulWidget {
   @override
   _SearchPageState createState() => _SearchPageState();
-
 }
 
 class _SearchPageState extends State<SearchPage> {
-  final PageController _controller = PageController(
-    initialPage: 0,
-  );
+  String showText = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Text('查找'),
-        )
+
+      body: Column(
+        children: <Widget>[
+          _appBar(),
+          SearchBar(
+            hideLeft: true,
+            defaultText: '1111',
+            hint: '123',
+            leftButtonClick: () {
+              Navigator.pop(context);
+            },
+            onChanged: _onTextChange,
+          ),
+          InkWell(
+            onTap: () {
+              SearchDao.fetch('')
+                  .then((SearchModel value) {
+                setState(() {
+                  showText = value.data[0].url;
+                });
+              });
+            },
+            child: Text('Get'),
+          ),
+          Text(showText)
+        ],
+      ),
     );
   }
 
+  _onTextChange(text) {}
+  _appBar
 }
